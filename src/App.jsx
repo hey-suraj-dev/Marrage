38c215f745f387c8c85cc58930f9e8f5454ce931
Suraj <koli82251@gmail.com>
2026-09-11 15:43:02 +0530

    Fix: Remove Gallery import, add root element check, simplify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Events from './components/Events';
-import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import CountdownTimer from './components/CountdownTimer';
 import AppreciationWall from './components/AppreciationWall';
 import Minvitation from './components/Minvitation';
 import InvitationCard from './components/InvitationCard';
+
 function App() {
   return (
     <div className="bg-gradient-to-b from-black to-gray-800 min-h-screen text-white">
@@ -17,7 +16,6 @@ function App() {
       <InvitationCard />
       <About />
       <Events />
-     {/* <Gallery /> */} 
       <AppreciationWall />
       <Footer />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  console.error('Root element not found!')
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
